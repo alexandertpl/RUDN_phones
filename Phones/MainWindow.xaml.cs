c57282ab875a24e338b612a416a5a1e710868c04
CyberTahir <HairulGang@yandex.ru>
2025-03-21 01:48:08 +0300

    Добавьте файлы проекта.

---
+++ Phones/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Phones
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PhonesViewModel phonesViewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            phonesViewModel = new PhonesViewModel("C:\\Users\\User\\source\\repos\\Phones\\Phones\\phonesList.json");
+            DataContext = phonesViewModel;
+        }
+
+        private void Price_Edit(object sender, RoutedEventArgs e)
+        {
+            int newPrice = int.Parse(PhonePrice.Text);
+            phonesViewModel.SelectedPhone.Price = newPrice;
+        }
+    }
+}
---
+++ Phones/PhonesViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+namespace Phones
+{
+    public class PhonesViewModel : INotifyPropertyChanged
+    {
+        private Phone selectedPhone;
+        public ObservableCollection<Phone> PhonesList { get; set; }
+        public Phone SelectedPhone
+        {
+            get { return selectedPhone; }
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public PhonesViewModel(string phonesListFilePath)
+        {
+            FileStream fileStream = File.OpenRead(phonesListFilePath);
+            PhonesList = JsonSerializer.Deserialize<ObservableCollection<Phone>>(fileStream);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop="")
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
